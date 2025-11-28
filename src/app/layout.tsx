74e4daf376cb438b4fc72275fc8e78eb7c1ae9bc
Jeremy Jason Driss <mmesgaimco@gmail.com>
2025-11-28 15:22:27 +0000

    Ça ne fonctionne pas. Peux-tu utiliser le lien d’une image d’une plante

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,10 @@ export default function RootLayout({
     <html lang="fr" className="h-full" suppressHydrationWarning>
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/icon-512x512.png" />
+        <link rel="icon" href="https://res.cloudinary.com/dlhbpswom/image/upload/v1717769586/favicon_a2k3k2.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="https://res.cloudinary.com/dlhbpswom/image/upload/v1717769586/apple-touch-icon_b1b2c3.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="https://res.cloudinary.com/dlhbpswom/image/upload/v1717769586/icon-192x192_m1vj3t.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="https://res.cloudinary.com/dlhbpswom/image/upload/v1717769586/icon-512x512_q6z8ab.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
