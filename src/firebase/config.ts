45880283968846efd9f52ecd7dd5bda32b11e9fd
Jeremy Jason Driss <mmesgaimco@gmail.com>
2025-11-28 00:40:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5811372516-7862f",
+  "appId": "1:299550704548:web:8bc2f68def0aec6002b731",
+  "apiKey": "AIzaSyDHEGwyvqV_4uGC486m1362dvK54Q9VpWQ",
+  "authDomain": "studio-5811372516-7862f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "299550704548"
+};
