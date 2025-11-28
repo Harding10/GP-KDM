9b1d3d8386fc1f8e071e44aadc17a166292017f3
Jeremy Jason Driss <mmesgaimco@gmail.com>
2025-11-28 16:08:17 +0000

    ESAYE AVEC CETTE IMAGE QUI EST EN 512X512 : https://play-lh.googleuserco

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,10 @@ export default function RootLayout({
     <html lang="fr" className="h-full" suppressHydrationWarning>
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="https://res.cloudinary.com/dlhbpswom/image/upload/v1717769586/favicon_a2k3k2.ico" sizes="any" />
-        <link rel="apple-touch-icon" href="https://res.cloudinary.com/dlhbpswom/image/upload/v1717769586/apple-touch-icon_b1b2c3.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="https://res.cloudinary.com/dlhbpswom/image/upload/v1717769586/icon-192x192_m1vj3t.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="https://res.cloudinary.com/dlhbpswom/image/upload/v1717769586/icon-512x512_q6z8ab.png" />
+        <link rel="icon" href="https://play-lh.googleusercontent.com/OocMG8bjiq6hNmg2LMNrdcSlZgBfqc6b-0erv4IE8wlR88MgCWPZS_Te42iR5UV7sA" sizes="any" />
+        <link rel="apple-touch-icon" href="https://play-lh.googleusercontent.com/OocMG8bjiq6hNmg2LMNrdcSlZgBfqc6b-0erv4IE8wlR88MgCWPZS_Te42iR5UV7sA" />
+        <link rel="icon" type="image/png" sizes="192x192" href="https://play-lh.googleusercontent.com/OocMG8bjiq6hNmg2LMNrdcSlZgBfqc6b-0erv4IE8wlR88MgCWPZS_Te42iR5UV7sA" />
+        <link rel="icon" type="image/png" sizes="512x512" href="https://play-lh.googleusercontent.com/OocMG8bjiq6hNmg2LMNrdcSlZgBfqc6b-0erv4IE8wlR88MgCWPZS_Te42iR5UV7sA" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
