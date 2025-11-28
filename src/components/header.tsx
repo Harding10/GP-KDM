e4f7c77d71b608331d150153c3dab82bdcbae4c1
Jeremy Jason Driss <mmesgaimco@gmail.com>
2025-11-28 11:41:36 +0000

    peaufiner le design

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,8 @@
+
 'use client';
 
 import Link from 'next/link';
 import UserAuthButton from './user-auth-button';
-
-const AppLogo = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="h-8 w-8 text-primary"
-  >
-    <path d="M12 2a7 7 0 0 0-7 7c0 5 7 13 7 13s7-8 7-13a7 7 0 0 0-7-7z" />
-    <path d="M12 9a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
-  </svg>
-);
-
 
 const CustomLogo = () => (
     <svg 
